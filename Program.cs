be874c462bbcdddd44d28f871527e66f9fa44584
Dzejkoubi <113968460+Dzejkoubi@users.noreply.github.com>
2022-11-08 09:10:50 +0100

    first commint

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string vstup = "0";
+long cislo  = 0;
+while (vstup != "n")
+{
+    vstup = Console.ReadLine();
+    if (vstup != "n")
+    {
+        cislo = long.Parse(vstup);
+            for (int i = 0; i < cislo; i++)
+            {
+                Console.WriteLine("Cyklus: " + i);
+            }
+    }
+}
+
+
+Console.ReadKey();
